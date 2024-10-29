e4aaa33c6efffae878c79b86625d2a8d171c5e0a
Ariel Schmeing <ariel.schmeing@proton.me>
2024-10-29 10:20:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+# inicializacao da tela
+screen_size: tuple = (800, 800)
+screen = pygame.display.set_mode(screen_size)
+
+pygame.display.set_caption('Game Test Alpha')
+
