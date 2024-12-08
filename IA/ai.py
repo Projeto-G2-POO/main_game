4f464e96ee36ed2a8585f4f3ccc99bdd06654a9b
Guilherme Benelli <164866820+Benelli77@users.noreply.github.com>
2024-12-08 16:46:24 -0300

    ajuste com uns comments sobre alguns codigos para rodar

--- IA/ai.py
+++ IA/ai.py
@@ -1,3 +1,20 @@
+#$response = Invoke-WebRequest -Uri "http://127.0.0.1:8000/" -Method POST -Headers @{
+>>#     "Content-Type" = "application/json"
+>> #} -Body '{"text": "answer accordingly: imagine you are on a medieval world, in a grass field, 
+#you are a traveler venturing from afar, answer propperly: imagine you are a goblin standing by my side, 
+#give a warm welcome to the  traveller. remember, we are in a open gras field, with monsters arriving soon."}'
+
+#uvicorn ---ai:app --reload---  starta o server e deixa ele atualizar em tempo real
+# ---$response.content---     mostra a resposta da llm inteira
+
+
+
+
+
+
+
+
+
 from fastapi import FastAPI, HTTPException 
 import ollama
 from pydantic import BaseModel 
